dbd94abcbfd8846cdbf05c8682be94e5c4e504ca
Tim Edwards <timedwards121895@gmail.com>
2019-05-02 12:14:09 -0400

    Add files via upload

---
+++ FRA/dfv.py
@@ -0,0 +1,96 @@
+#Author: Tim Edwards
+
+# import the necessary packages
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import time
+import cv2
+import os
+import argparse
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--name", required=True,
+	help="name of user")
+args = vars(ap.parse_args())
+
+savePath = "dataset/" 
+name = args["name"]
+savePath = savePath + name 
+
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe("deploy.prototxt.txt", "res10_300x300_ssd_iter_140000.caffemodel")
+
+# initialize the video stream and allow the cammera sensor to warmup
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+total = 0
+imagesCollected = 0
+# loop over the frames from the video stream
+
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+    total +=1
+ 
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+                                 (300, 300), (104.0, 177.0, 123.0))
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    net.setInput(blob)
+    detections = net.forward()
+    imagePath = savePath + "/face{}.jpg".format(str(total).zfill(5))
+    
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence < 0.5:    
+            continue
+
+		# compute the (x, y)-coordinates of the bounding box for the
+		# object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+            
+		# draw the bounding box of the face along with the associated
+		# probability
+        text = "{:.2f}%".format(confidence * 100)
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("f"):
+            imagesCollected += 1
+            if not os.path.isdir(savePath):
+                os.makedirs(savePath)
+            face = frame[startY-30:endY+30, startX-30:endX+30]
+            cv2.imwrite(imagePath, face)
+        
+        cv2.rectangle(frame, (startX, startY), (endX, endY),
+                      (0, 0, 255), 2)
+        cv2.putText(frame, text, (startX, y),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        
+		# compute the (x, y)-coordinates of the bounding box for the
+		# object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+	# show the output frame
+    frame = imutils.resize(frame, width = 600)
+    cv2.imshow("Frame", frame)
+    if key == ord("q"):
+        break
+     
+print (imagesCollected,"images collected")
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
